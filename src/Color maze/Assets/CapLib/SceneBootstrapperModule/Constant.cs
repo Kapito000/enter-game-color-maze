075db403ea4e77ae701721043c9e386da39ab56a
Egor <rkrivzha@yandex.ru>
2025-04-25 10:35:06 +0300

    SceneBootstrapper.

---
+++ src/Color maze/Assets/CapLib/SceneBootstrapperModule/Constant.cs
@@ -0,0 +1,11 @@
+﻿using static CapLib.Common.Constant;
+
+namespace CapLib.SceneBootstrapper
+{
+	public static class Constant
+	{
+		public const string ModuleName = "Scene bootstrapper";
+		public const string FullModuleName =
+			FullLibName + ModuleName + ModuleSeparator;
+	}
+}
