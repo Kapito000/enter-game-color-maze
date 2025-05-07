bc9f059b63c77bf91cb8a184701f51dbbb067c96
Egor <rkrivzha@yandex.ru>
2025-05-07 10:40:38 +0300

    CapLib.Debugger.

---
+++ src/Color maze/Assets/CapLib/Debug/Debugger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CapLib.StateMachine
+{
+	public class Debugger : IDebugger
+	{
+		public void Log(string msg)
+		{
+			Debug.Log(msg);
+		}
+
+		public void Error(string msg)
+		{
+			Debug.LogError(msg);
+		}
+	}
+}
