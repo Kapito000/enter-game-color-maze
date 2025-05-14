1690991d8f3c35d4d0c7e18096cb496659a01db4
Egor <rkrivzha@yandex.ru>
2025-05-14 10:18:36 +0300

    SerializedDictionary.

---
+++ src/Color maze/Assets/CapLib/Collection/SerializedDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CapLib.Collection
+{
+	public abstract class SerializedDictionary<TKey, TValue> :
+		Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+	{
+		[SerializeField, HideInInspector]
+		List<TKey> _keyData = new();
+
+		[SerializeField, HideInInspector]
+		List<TValue> _valueData = new();
+
+		void ISerializationCallbackReceiver.OnAfterDeserialize()
+		{
+			Clear();
+			for (int i = 0; i < _keyData.Count && i < _valueData.Count; i++)
+			{
+				this[_keyData[i]] = _valueData[i];
+			}
+		}
+
+		void ISerializationCallbackReceiver.OnBeforeSerialize()
+		{
+			_keyData.Clear();
+			_valueData.Clear();
+
+			foreach (var item in this)
+			{
+				_keyData.Add(item.Key);
+				_valueData.Add(item.Value);
+			}
+		}
+	}
+}
